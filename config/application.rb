efdb209214ca6497bc5311d61626ab87c6ba3661
MajorMetal <MajorMetal@clownfart>
2014-09-02 18:09:20 -0700

    done

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.serve_static_assets = true
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module DomViolenceApp
   class Application < Rails::Application
+
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
